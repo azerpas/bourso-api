6384d6b700f6312320757ce35183e15744468d63
azerpas <xskylix@hotmail.fr>
2023-10-22 07:46:12 +0200

    feat: config subcommand

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use anyhow::{Result, Context};
 use clap::{Arg, Command};
 
 mod bourso;
+mod settings;
+
+use settings::{Settings, get_settings, save_settings};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -18,13 +21,17 @@ async fn main() -> Result<()> {
                 .about("Manage your saving accounts")
 
         )
-        .args([
-            Arg::new("username")
-                .short('u')
-                .long("username")
-                .help("Your Boursorama username")
-                .required(true),
-        ])
+        .subcommand(
+            Command::new("config")
+                .about("Configure BoursoBank/Boursorama CLI")
+                .arg(
+                    Arg::new("username")
+                        .short('u')
+                        .long("username")
+                        .help("Your customer id")
+                        .required(true)
+                )
+        )
         .get_matches();
 
     match matches.subcommand() {
@@ -37,8 +44,32 @@ async fn main() -> Result<()> {
         Some(("balance", _)) => {
             println!("login");
         }
+        Some(("config", config_matches)) => {
+            let customer_id = config_matches.get_one::<String>("username").map(|s| s.as_str()).unwrap();
+            save_settings(&Settings { customer_id: Some(customer_id.to_string()) })?;
+            println!("Configuration saved");
+            return Ok(());
+        }
         _ => unreachable!(),
     }
+
+    let settings = get_settings()?;
+
+    if settings.customer_id.is_none() {
+        println!("Please configure your customer id with `bourso config --username <customer_id>`");
+        return Ok(());
+    }
+    let customer_id = settings.customer_id.unwrap();
+
+    // Get password from stdin
+    let password = rpassword::prompt_password("Your password: ")
+        .context("Failed to read password")?
+        .trim()
+        .to_string();
+
+    let mut web_client: bourso::client::BoursoWebClient = bourso::get_client();
+    web_client.init_session().await?;
+    web_client.login(&customer_id, &password).await?;
 
     Ok(())
 }
