7c0650c6df40f70169b3c0b03d6ecf2e4f3cf3ac
azerpas <manikhouth.anthony@gmail.com>
2025-03-10 20:00:43 +0100

    feat: add get trading summary

--- src/bourso_api/src/client/trade/mod.rs
+++ src/bourso_api/src/client/trade/mod.rs
@@ -2,17 +2,36 @@ pub mod error;
 pub mod order;
 pub mod tick;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
+use serde::{Deserialize, Serialize};
 
 use crate::account::{Account, AccountKind};
 
-use super::{
-    BoursoWebClient,
-    config::Config,
-};
+use super::{config::Config, BoursoWebClient};
 
 impl BoursoWebClient {
     pub async fn trade() {}
+    pub async fn get_trading_summary(&self, account: Account) -> Result<Vec<TradingSummaryItem>> {
+        let url = get_trading_summary_url(&self.config, account)?;
+        let response = self.client.get(url).send().await?;
+
+        let status_code = response.status();
+
+        let response = response.text().await?;
+
+        if status_code != 200 {
+            return Err(anyhow::anyhow!(
+                "Failed to get trading summary response: {}",
+                response
+            ));
+        }
+
+        let summary: Vec<TradingSummaryItem> = serde_json::from_str(&response).context(format!(
+            "Failed to parse trading summary response. Response: {}",
+            response
+        ))?;
+        Ok(summary)
+    }
 }
 
 fn get_trading_base_url(config: &Config) -> Result<String> {
@@ -20,13 +39,11 @@ fn get_trading_base_url(config: &Config) -> Result<String> {
         return Err(anyhow::anyhow!("User hash is not set"));
     }
 
-    Ok(
-        format!(
-            "{}/_user_/_{}_/trading",
-            config.api_url,
-            config.user_hash.as_ref().unwrap()
-        )
-    )
+    Ok(format!(
+        "{}/_user_/_{}_/trading",
+        config.api_url,
+        config.user_hash.as_ref().unwrap()
+    ))
 }
 
 fn get_trading_summary_url(config: &Config, account: Account) -> Result<String> {
@@ -44,10 +61,65 @@ fn get_trading_summary_url(config: &Config, account: Account) -> Result<String> 
 }
 
 fn get_trading_is_first_order_url(config: &Config) -> Result<String> {
-    Ok(
-        format!(
-            "{}/order/isfirstorder?_host=tradingboard.boursobank.com",
-            get_trading_base_url(config)?
-        )
-    )
+    Ok(format!(
+        "{}/order/isfirstorder?_host=tradingboard.boursobank.com",
+        get_trading_base_url(config)?
+    ))
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TradingSummaryItem {
+    /// Either "account" or "positions"
+    pub id: String,
+    pub positions: Option<Vec<PositionSummary>>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PositionSummary {
+    /// The symbol of the position ie. 1rTCW8
+    pub symbol: String,
+    /// The name of the position ie. AMUNDI ETF MSCI WORLD UCITS ETF
+    pub label: String,
+    pub permalink: String,
+    pub quantity: SummaryValue,
+    pub buying_price: SummaryValue,
+    pub amount: SummaryValue,
+    pub last: SummaryValue,
+    /// Variation ?
+    pub var: SummaryValue,
+    pub gain_loss: SummaryValue,
+    pub gain_loss_percent: SummaryValue,
+    /// YYYY-MM-DD
+    pub last_movement_date: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SummaryValue {
+    pub value: f64,
+    pub decimals: u64,
+    pub currency: Option<String>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AccountSummary {
+    /// Name of the account
+    pub name: String,
+    pub currency: String,
+    /// eg "TRADING"
+    pub type_category: String,
+    /// YYYY-MM-DD
+    pub activation_date: String,
+    pub balance: SummaryValue,
+    pub cash: SummaryValue,
+    pub valuation: SummaryValue,
+    pub total: SummaryValue,
+    pub gain_loss: SummaryValue,
+    pub gain_loss_percent: SummaryValue,
+    pub liquidation_amount: SummaryValue,
+    /// Cash deposited
+    pub contribution: i64,
 }
